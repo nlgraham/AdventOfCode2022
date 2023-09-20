0522b26ce4a3c5d97d2fd679a83d54518bc0b05f
Natasha Graham <nlgraham95@gmail.com>
2023-09-19 20:31:52 -0500

    Add project files.

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,54 @@
+﻿/* Day1.cs
+ * Author: Natasha Graham
+ * Created 2023-09-19
+ */
+
+
+int[] maxCalories = { 0, 0, 0 };
+int sum = 0;
+
+String? input = Console.ReadLine();
+
+while(input != null)
+{
+    Array.Sort(maxCalories);
+    if (input.Equals("end"))
+        break;
+    if(input.Equals(""))
+    {
+        if (sum > maxCalories[0])
+        {
+            if (sum > maxCalories[1])
+            {
+                if(sum > maxCalories[2])
+                {
+                    maxCalories[0] = maxCalories[1];
+                    maxCalories[1] = maxCalories[2];
+                    maxCalories[2] = sum;
+                }
+                else
+                {
+                    maxCalories[0] = maxCalories[1];
+                    maxCalories[1] = sum;
+                }
+            }
+            else
+            {
+                maxCalories[0] = sum;
+            }
+        }
+        sum = 0;
+        input = Console.ReadLine();
+        //Array.Sort(maxCalories);
+        continue;
+    }
+
+    sum += Int32.Parse(input);
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(maxCalories[0] + maxCalories[1] + maxCalories[2]);
+
+
+// part 1 answer: 70764
+// part 2 answer 203905
---
+++ Day2-RockPaperScissors/Day2.cs
@@ -0,0 +1,48 @@
+﻿/* Day2.cs
+ * Author: Natasha Graham
+ * Created 2023-09-19
+ */
+
+String? match = Console.ReadLine();
+
+int score = 0;
+
+while (!match.Equals("end"))
+{
+    switch(match)
+    { 
+        case "A X": // rock = rock tie
+            score += 1 + 3;
+            break;
+        case "A Y": // rock < paper win
+            score += 2 + 6;
+            break;
+        case "A Z": // rock > scissors loss
+            score += 3 + 0;
+            break;
+        case "B X": // paper > rock loss
+            score += 1 + 0;
+            break;
+        case "B Y": // paper = paper tie
+            score += 2 + 3;
+            break;
+        case "B Z": // paper < scissors win
+            score += 3 + 6;
+            break;
+        case "C X": // scissors < rock win
+            score += 1 + 6;
+            break;
+        case "C Y": // scissors > paper loss
+            score += 2 + 0;
+            break;
+        case "C Z": // scissors = scissors tie
+            score += 3 + 3;
+            break;
+    }
+
+    match = Console.ReadLine();
+}
+
+Console.WriteLine(score);
+
+// part 1 answer 9651
