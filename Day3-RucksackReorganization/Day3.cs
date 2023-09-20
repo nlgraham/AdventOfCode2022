df200f3fa0b275ff1b159f4743bdca1d29826149
Natasha Graham <nlgraham95@gmail.com>
2023-09-20 17:09:57 -0500

    day 3 part 2

--- Day3-RucksackReorganization/Day3.cs
+++ Day3-RucksackReorganization/Day3.cs
@@ -7,10 +7,13 @@ char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
     'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 
-String? input = Console.ReadLine();
+char[] bag1 = Console.ReadLine().ToCharArray();
+char[] bag2 = Console.ReadLine().ToCharArray();
+char[] bag3 = Console.ReadLine().ToCharArray();
 
 int sum = 0;
 
+/*
 while(!input.Equals("end"))
 {
     int halfPoint = input.Length / 2;
@@ -32,7 +35,32 @@ while(!input.Equals("end"))
 
     input = Console.ReadLine();
 }
+*/
+
+while(true)
+{
+    foreach(char c in bag1)
+    {
+        if(bag2.Contains(c))
+        {
+            if(bag3.Contains(c))
+            {
+                sum += Array.IndexOf(letters, c) + 1;
+                break;
+            }
+        }
+    }
+
+    bag1 = Console.ReadLine().ToCharArray();
+    if(bag1.Length == 3 && bag1[0] == 'e')
+    {
+        break;
+    }
+    bag2 = Console.ReadLine().ToCharArray();
+    bag3 = Console.ReadLine().ToCharArray();
+}
 
 Console.WriteLine(sum);
 
 // 7616 too low - part 1 answer 7990
+// 2602 for part 2
