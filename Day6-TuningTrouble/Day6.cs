bf8cbfd700ac3b83d3b9d68ab644ca9b787801e1
Natasha Graham <nlgraham95@gmail.com>
2023-09-24 15:29:11 -0500

    day 6 part 1

---
+++ Day6-TuningTrouble/Day6.cs
@@ -0,0 +1,35 @@
+﻿/* Day6.cs
+ * Author: Natasha Graham
+ * Created 2023-09-24
+ */
+
+
+// Get input from console, convert to ReadOnlySpan
+ReadOnlySpan<char> stream = Console.ReadLine().AsSpan();
+
+// initialize int for final result
+int index = 0;
+
+
+// Run through set of four characters in a row, starting with index 3 to the end
+// Each set of four is added to a HashSet
+// Which is then checked for four unique entries
+// Upon which the current index + one is given to the answer int
+// and the loop is broken
+for(int i = 3; i < stream.Length; i++)
+{
+    HashSet<char> set = new HashSet<char>();
+
+    foreach (char c in stream[(i - 3)..(i + 1)])
+    {
+        set.Add(c);
+    }
+
+    if(set.Count == 4)
+    {
+        index = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine(index);
