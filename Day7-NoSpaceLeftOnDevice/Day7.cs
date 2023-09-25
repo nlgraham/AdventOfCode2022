00fa3d03202ab7ff93beee56b9d4e9d31488c36f
Natasha Graham <nlgraham95@gmail.com>
2023-09-24 22:13:36 -0500

    day 7 part 1

---
+++ Day7-NoSpaceLeftOnDevice/Day7.cs
@@ -0,0 +1,62 @@
+﻿/* Day7.cs
+ * Author: Natasha Graham
+ * Created 2023-09-24
+ */
+
+using Day7_NoSpaceLeftOnDevice;
+
+
+
+String[] input = Console.ReadLine().Split(' ');
+TreeNode currentNode = new TreeNode(input[2]);
+
+TreeNode root = currentNode;
+
+int totalSystemSize = 70000000;
+int minUpdateSpace = 30000000;
+
+
+while(input.Length > 1)
+{
+    input = Console.ReadLine().Split(' ');
+
+    if (input.Length == 1)
+        break;
+
+    if (input[0].Equals("$"))
+    {
+        if(input[1].Equals("ls"))
+        {
+            continue;
+        }
+
+        if (input[2].Equals(".."))
+        {
+            currentNode = currentNode.Parent;
+            continue;
+        }
+
+        currentNode = currentNode.Children.First(c => c.Name == input[2]);
+    }
+    else if(input[0].Equals("dir"))
+    {
+        currentNode.AddChild(new TreeNode(input[1], currentNode));
+    }
+    else
+    {
+        currentNode.AddChild(new TreeNode(input[1], Int32.Parse(input[0]), currentNode));
+    }
+}
+
+List<int> sizes = new List<int>();
+
+int rootSize = root.FindDirSize(100000, sizes);
+
+int sumOfSizes = 0;
+
+foreach(int s in sizes)
+{
+    sumOfSizes += s;
+}
+
+Console.WriteLine(sumOfSizes);
---
+++ Day7-NoSpaceLeftOnDevice/TreeNode.cs
@@ -0,0 +1,78 @@
+﻿/* TreeNode.cs
+ * Author: Natasha Graham
+ * Created 2023-09-24
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_NoSpaceLeftOnDevice
+{
+    internal class TreeNode
+    {
+        public TreeNode(String name)
+        {
+            Name = name;
+            IsDir = true;
+            Size = 0;
+        }
+
+        public TreeNode(String name, int size, TreeNode parent)
+        {
+            Name = name;
+            Size = size;
+            Parent = parent;
+            IsDir = false;
+        }
+
+        public TreeNode(String name, TreeNode parent)
+        {
+            Name = name;
+            Parent = parent;
+            IsDir = true;
+            Size = 0;
+        }
+
+        public String Name { get; set; }
+        public int Size { get; set; }
+        public TreeNode Parent { get; set; }
+        public bool IsDir { get; set; }
+        public List<TreeNode> Children { get; set;} = new List<TreeNode>();
+
+        public void AddChild(TreeNode child)
+        {
+            if (IsDir)
+            {
+                Children.Add(child);
+            }
+        }
+
+        public void UpdateSize(int s)
+        {
+            Size += s;
+        }
+
+        public int FindDirSize(int maxSize, List<int> finalNodes)
+        {
+            foreach(TreeNode child in Children)
+            {
+                if(child.IsDir)
+                { Size += child.FindDirSize(maxSize, finalNodes); }
+                else
+                {
+                    Size += child.Size;
+                }
+            }
+
+            if(Size <= maxSize)
+            {
+                finalNodes.Add(Size);
+            }
+
+            return Size;
+        }
+    }
+}
