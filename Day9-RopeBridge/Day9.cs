92fd2d0b9f436df18a1889a248ea842568d7ea76
Natasha Graham <nlgraham95@gmail.com>
2023-09-26 18:54:56 -0500

    started day 9

---
+++ Day9-RopeBridge/Day9.cs
@@ -0,0 +1,121 @@
+﻿/* Day9.cs
+ * Author: Natasha Graham
+ * Created 2023-09-25
+ */
+
+using Day9_RopeBridge;
+
+
+OrderedPair head = new OrderedPair(0, 0);
+OrderedPair tail = new OrderedPair(0, 0);
+HashSet<OrderedPair> visited = new HashSet<OrderedPair>();
+
+visited.Add(new OrderedPair(tail.X, tail.Y));
+
+string[] input = Console.ReadLine().Split(' ');
+
+while(input.Length > 1)
+{
+    char direction = input[0][0];
+    int times = Int32.Parse(input[1]);
+
+    for(int i = times; i > 0; i--)
+    {
+        switch(direction)
+        {
+            case 'R':
+                head.UpdateX(1);
+                break;
+            case 'L':
+                head.UpdateX(-1);
+                break;
+            case 'U':
+                head.UpdateY(1);
+                break;
+            case 'D':
+                head.UpdateY(-1);
+                break;
+        }
+
+        if (!isNextTo(head, tail))
+        {
+            if ((tail.X - head.X) < -1)
+            {
+                if ((tail.Y - head.Y) == 1)
+                {
+                    tail.UpdateY(-1);
+                    tail.UpdateX(1);
+                }
+                else if ((tail.Y - head.Y) == -1)
+                {
+                    tail.UpdateY(1);
+                    tail.UpdateX(1);
+                }
+                else
+                    tail.UpdateX(1);
+            }
+            else if ((tail.X - head.X) > 1)
+            {
+                if ((tail.Y - head.Y) == 1)
+                {
+                    tail.UpdateY(-1);
+                    tail.UpdateX(-1);
+                }
+                else if ((tail.Y - head.Y) == -1)
+                {
+                    tail.UpdateY(1);
+                    tail.UpdateX(-1);
+                }
+                else
+                    tail.UpdateX(-1);
+            }
+            else if ((tail.Y - head.Y) > 1)
+            {
+                if((tail.X - head.X) == 1)
+                {
+                    tail.UpdateY(-1);
+                    tail.UpdateX(-1);
+                }
+                else if ((tail.X - head.X) == -1)
+                {
+                    tail.UpdateY(-1);
+                    tail.UpdateX(1);
+                }
+                else
+                    tail.UpdateY(-1);
+            }
+            else if ((tail.Y - head.Y) < -1)
+            {
+                if((tail.X - head.X) == 1)
+                {
+                    tail.UpdateY(1);
+                    tail.UpdateX(-1);
+                }
+                else if ((tail.X - head.X) == -1)
+                {
+                    tail.UpdateY(1);
+                    tail.UpdateX(1);
+                }
+                else
+                    tail.UpdateY(1);
+            }
+        }
+
+        visited.Add(new OrderedPair(tail.X, tail.Y));
+    }
+
+    input = Console.ReadLine().Split(' ');
+}
+
+
+
+
+bool isNextTo(OrderedPair head, OrderedPair tail)
+{
+    if (Math.Abs(tail.X - head.X) <= 1 && Math.Abs(tail.Y - head.Y) <= 1)
+        return true;
+
+    return false;
+}
+
+Console.WriteLine(visited.Count);
---
+++ Day9-RopeBridge/OrderedPair.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_RopeBridge
+{
+    internal class OrderedPair
+    {
+        public OrderedPair(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void UpdateX(int addend)
+        {
+            X += addend;
+        }
+        public void UpdateY(int addend)
+        {
+            Y += addend;
+        }
+
+        public bool Equals(OrderedPair pair)
+        {
+            return X == pair.X && Y == pair.Y;
+        }
+
+        public int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+    }
+}
